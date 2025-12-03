276276349dcb283496d1f230d8994bc1bf487984
MGuadalupeCc23 <147435424+MGuadalupeCc23@users.noreply.github.com>
2025-12-03 03:46:27 +0000

    fixed about of main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,10 @@
-/*const BASE_PATH = window.location.hostname.includes("github.io")
-    ? "/figuras_planas"
-    : "";
+const BASE_PATH = window.location.hostname.includes("github.io")
+  ? "/figuras_planas"
+  : "";
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadHeader();
-  loadFooter();
-
-  // Lógica del Service Worker
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register(`${BASE_PATH}/sw.js`)
-      .then((registration) => {
-        console.log("ServiceWorker registrado con scope:", registration.scope);
-      })
-      .catch((err) => {
-        console.error("Fallo al registrar ServiceWorker:", err);
-      });
-  }
-
-  console.log("Módulo cargado correctamente");
-});
-
-// 🔹 Cargar Sidebar
-export async function loadHeader() {
-  const header = document.getElementById("sideBar");
-  const response = await fetch(`${BASE_PATH}/partials/sideBar.html`);
-  header.innerHTML = await response.text();
-}
-
-// 🔹 Cargar Footer
-export async function loadFooter() {
-  const footer = document.getElementById("footer");
-  const response = await fetch(`${BASE_PATH}/partials/footer.html`);
-  footer.innerHTML = await response.text();
-}
-*/
-document.addEventListener("DOMContentLoaded", () => {
-  // tu lógica nueva
   loadSideBar();
   loadFooter();
-  //Lógica del service worker
-  const BASE_PATH = window.location.hostname.includes("github.io")
-    ? //? "/PWA" // nombre exacto del repositorio
-      "/figuras_planas" // nombre exacto del repositorio
-    : ""; // en local (XAMPP)
 
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
@@ -55,6 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error("Fallo al registrar ServiceWorker:", err);
       });
   }
+
   console.log("Módulo cargado correctamente");
 });
 
